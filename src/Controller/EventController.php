48c3335e477d426175cde646dc35648b8af7d018
valentinmougenot <valentin.mougenot2003@outlook.com>
2024-06-08 15:26:56 +0200

    feat: Added sidebar for events (DOK-1)

---
+++ src/Controller/EventController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/event', name: 'app_event')]
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    #[Route('/event/create', name: 'app_event_create')]
+    public function create(): Response
+    {
+        return $this->render('event/create.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    #[Route('/event/list', name: 'app_event_list')]
+    public function list(): Response
+    {
+        return $this->render('event/list.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    #[Route('/event/register', name: 'app_event_register')]
+    public function register(): Response
+    {
+        return $this->render('event/register.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+}
