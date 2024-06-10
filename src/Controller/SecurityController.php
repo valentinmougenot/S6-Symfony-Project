98dcc6de72c2244a1ad8af44fd761b17e7c9ebbf
MaxencePaulin <maxence.paulin25@gmail.com>
2024-06-10 22:11:18 +0200

    feat: Add profile page, editing profile, change password (missing style)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileFormType;
+use App\Form\ProfilePasswordFormType;
+use App\Security\AppCustomAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -28,5 +37,75 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('security/profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route(path: '/profile/edit', name: 'app_profile_edit')]
+    public function profileEdit(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+        
+        $errors = $form->getErrors(true, false);
+        
+        if ($form->isSubmitted() && $form->isValid() && $errors->count() === 0) {
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $security->login($user, AppCustomAuthenticator::class, 'main');
+        }
+        return $this->render('security/profile_edit.html.twig', [
+            'editForm' => $form,
+        ]);
+    }
+
+    #[Route(path: '/profile/password', name: 'app_profile_password')]
+    public function profilePassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(ProfilePasswordFormType::class, $user);
+        $form->handleRequest($request);
+
+        $errors = $form->getErrors(true, false);
+
+        if ($form->isSubmitted() && $form->isValid() && $errors->count() === 0) {
+            // encode the plain password
+            if ($form->get('plainPassword')->getData() === $form->get('confirmPassword')->getData()) {
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+    
+                $entityManager->persist($user);
+                $entityManager->flush();
+    
+                // do anything else you need here, like send an email
+    
+                return $security->login($user, AppCustomAuthenticator::class, 'main');
+            } 
+            $form->get('confirmPassword')->addError(new FormError('Les mots de passe ne correspondent pas'));
+        }
+
+        return $this->render('security/profile_password.html.twig', [
+            'passwordForm' => $form,
+        ]);
     }
 }
