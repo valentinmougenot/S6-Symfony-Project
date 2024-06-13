eccc37784b2eaadbe781084db99ea83e5b81cf1d
MaxencePaulin <maxence.paulin25@gmail.com>
2024-06-13 15:34:26 +0200

    feat: Add some style, redirection logout

--- src/Form/ProfilePasswordFormType.php
+++ src/Form/ProfilePasswordFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -16,46 +17,15 @@ class ProfilePasswordFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'label' => 'Mot de passe',
-                'attr' => ['autocomplete' => 'new-password',
-                'novalidate' => 'novalidate'],
+                'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                    // Mot de passe d'au moins 8 caractères, incluant chiffres et lettres.
-                    new Regex([
-                        'pattern' => '/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/',
-                        'message' => 'Votre mot de passe doit contenir au moins 8 caractères, incluant chiffres et lettres.',
-                    ]),
-                ],
-            ])
-            ->add('confirmPassword', PasswordType::class, [
-                'mapped' => false,
-                'label' => 'Confirmer le mot de passe',
-                'attr' => ['autocomplete' => 'new-password',
-                'novalidate' => 'novalidate'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez confirmer votre mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                    // Mot de passe d'au moins 8 caractères, incluant chiffres et lettres.
+                    // Mot de passe non vide d'au moins 8 caractères, incluant chiffres et lettres
                     new Regex([
                         'pattern' => '/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/',
                         'message' => 'Votre mot de passe doit contenir au moins 8 caractères, incluant chiffres et lettres.',
